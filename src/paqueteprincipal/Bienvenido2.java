0bdd6962bbf9114420af4a317f74adebf26257ec
javiersanchezmza <winamp4ever@gmail.com>
2018-11-11 08:21:40 -0300

    Initial commit

---
+++ src/paqueteprincipal/Bienvenido2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paqueteprincipal;
+
+/**
+ *
+ * @author Javier.Sanchez
+ * V2: Imprimo 2 líneas pero en una sola, con \n
+ */
+public class Bienvenido2 {
+    /** El método main comienza la ejecución del programa 
+     * 
+     * @param args 
+     */
+    public static void main( String args[] ){
+    System.out.println("Bienvenid@ a \nla programación en Java");
+    //System.out.println("la programación en Java");
+    }
+}
